fb20abf8456a499525d9d28166d38b6d34ab74cf
sprieton <santiagoprinun@gmail.com>
2022-02-08 18:57:55 +0100

    Plantilla test
    Signed-off-by: sprieton <santiagoprinun@gmail.com>

---
+++ src/test/bumpGo_test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "ros/ros.h"
+#include "fsm_bump_go/BumpGo.h"
+
+TEST(bumpGo_test, test_set_reading)
+{
+    // Heredar clases para simular salidas 
+    // Tests
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TEST();
+}
