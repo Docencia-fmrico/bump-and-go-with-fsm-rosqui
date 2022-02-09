8d11212c1bbdadafe6e5d37a504d80329c4d80ff
Gonzalo Vega <g.vega.2020@alumnos.urjc.es>
2022-02-09 11:31:54 +0100

    Suscribe y publica
    Signed-off-by: Gonzalo Vega <g.vega.2020@alumnos.urjc.es>

--- src/fsm_bump_go/BumpGo.cpp
+++ src/fsm_bump_go/BumpGo.cpp
@@ -22,18 +22,20 @@
 namespace fsm_bump_go
 {
 
+// Constructor
 BumpGo::BumpGo()
 : state_(GOING_FORWARD),
   pressed_(false)
 {
-  // sub_bumber_ = n_.subscribe(...);
-  // pub_vel_ = n_.advertise<...>(...)
+  // n_ es el NodeHandler. Se encarga de suscribir y publicar donde haga falta.
+  sub_bumber_ = n_.subscribe("/mobile_base/events/bumper", 1, &BumpGo::bumperCallback, this);
+  pub_vel_ = n_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",1);
 }
 
 void
 BumpGo::bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-  // pressed_ = (...);
+  pressed_ = true ;
   //  ...
 }
 
@@ -45,8 +47,8 @@ BumpGo::step()
   switch (state_)
   {
     case GOING_FORWARD:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
+      cmd.linear.x = 1;
+      cmd.angular.z = 0;
 
       if (pressed_)
       {
@@ -57,8 +59,8 @@ BumpGo::step()
 
       break;
     case GOING_BACK:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
+      cmd.linear.x = -1;
+      cmd.angular.z = 0;
 
       if ((ros::Time::now() - press_ts_).toSec() > BACKING_TIME )
       {
@@ -69,8 +71,8 @@ BumpGo::step()
 
       break;
     case TURNING:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
+      cmd.linear.x = 0;
+      cmd.angular.z = 1;
 
       if ((ros::Time::now()-turn_ts_).toSec() > TURNING_TIME )
       {
@@ -80,7 +82,7 @@ BumpGo::step()
       break;
     }
 
-    // pub_vel_.publish(...);
+    pub_vel_.publish(cmd);
 }
 
 }  // namespace fsm_bump_go
